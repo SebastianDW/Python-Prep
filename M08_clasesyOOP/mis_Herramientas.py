eb30263bb8a0e383016912a8a357e0b0a114e8bc
Sebastian <sebastian.dev.web@gmail.com>
2024-01-19 18:53:10 -0500

    modulo 8

---
+++ M08_clasesyOOP/mis_Herramientas.py
@@ -0,0 +1,78 @@
+class Herramientas:
+    def __init__(self, lista_numeros):
+        self.lista = lista_numeros
+        
+    def Verificar_primo(self,numero):
+        for a in range(2,(numero//2)+1):
+            if numero%a==0:
+                return False
+        return True
+    
+    def Valor_modal(self, lista):
+        frecuencia = {}
+
+        for elemento in lista: 
+            if elemento in frecuencia: 
+                frecuencia[elemento]+=1
+            else:
+                frecuencia[elemento]=1
+        valor_modal=None
+        frecuencia_maxima=0
+        for clave, frec in frecuencia.items():
+            if frec>frecuencia_maxima:
+                valor_modal=clave
+                frecuencia_maxima=frec
+        return valor_modal
+    def conversion_de_temperatura2(self, temperatura, temp_origen, temp_destino):
+        temp=0
+        if temp_origen=="farenheit":
+            if temp_destino=="celsius":
+                temp=(temperatura-32)*5/9
+            elif temp_destino=="kelvin":
+                temp=((temperatura-32)*5/9)+273.15
+        elif temp_origen== "kelvin":
+            if temp_destino=="celsius":
+                temp=temperatura-273.15
+            elif temp_destino=="farenheit":
+                temp= ((temperatura-273.15)*9/5)+32
+        elif temp_origen=="celsius":
+            if temp_destino=="farenheit":
+                temp=(temperatura*9/5)+32
+            elif temp_destino=="kelvin": 
+                temp=(temperatura+273.15)   
+            else:
+                print("ingrese una medida existente ")
+        return temp
+    def factorial(self, numero):
+        if isinstance(numero, int) and numero > 0:
+            if numero == 1:
+                return 1
+            else:
+                return numero * self.factorial(numero - 1)
+    
+    #creé un metodo para usar el resto de metodos para no cambiar el codigo de todos los metodos
+    def usar_Operaciones(self,opcion, t_origen="", t_destino=""):
+        
+        if opcion == 1 or opcion ==3 or opcion==4:
+            for i in self.lista:
+                if opcion==1:
+                    print(self.Verificar_primo(i))
+                elif opcion==3:
+                    print(self.conversion_de_temperatura2(i,t_origen,t_destino))
+                elif opcion == 4:
+                    print(self.factorial(i))
+        elif opcion == 2:
+            print(self.Valor_modal(self.lista))
+
+
+lista_prueba= [1,1,1,2,3,4,5,6,7,8,9,10]
+herramientas = Herramientas(lista_prueba)
+#se puede poner un input para preguntar que accion debería usar
+# 1 identificar primo 
+# 2 valor modal
+# 3 convertir tremperatura
+# 4 factorial
+herramientas.usar_Operaciones(1)
+herramientas.usar_Operaciones(2)
+herramientas.usar_Operaciones(3,"celsius","farenheit")
+herramientas.usar_Operaciones(4)
